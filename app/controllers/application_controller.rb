a0f59950cb625a799823de37f4b99e17bf025f77
gcalquin <gcalquin@gmail.com>
2023-05-23 23:50:15 -0400

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+#Opcion redireccion al iniciar session
+  #  def after_sign_in_path_for(resource)
+  #      posts_path
+  #      end
+       
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+
+
+  resources :posts
+  get '/posts', to: 'posts#index', as: 'user_root'
+
+  get 'home/index'
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "home#index"
+
+
+
+  
+
+
+end
